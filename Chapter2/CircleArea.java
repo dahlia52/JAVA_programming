d09d96577ca3b634bcb5debed208df38c54ca2c7
dahlia52 <101353482+dahlia52@users.noreply.github.com>
2022-09-16 12:18:18 +0900

    Add files via upload

---
+++ Chapter2/CircleArea.java
@@ -0,0 +1,13 @@
+// 2-3
+public class CircleArea {
+
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		double radius = 10.2;
+		double circleArea = radius * radius * PI;
+		
+		System.out.println
+		("반지름 "+radius +", 원의 면적 = "+circleArea);	
+	}
+}
+
