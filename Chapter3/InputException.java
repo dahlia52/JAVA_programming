577b01223da8afb2332685343d49ab25cf6ba7b6
dahlia52 <101353482+dahlia52@users.noreply.github.com>
2022-11-02 14:20:19 +0900

    Add files via upload

---
+++ Chapter3/InputException.java
@@ -0,0 +1,26 @@
+// 3-17
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class InputException {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("정수 3개를 입력하세요");
+		int sum = 0, n=0;
+		for (int i=0; i<3; i++) {
+			try {
+				System.out.print(i + ">>");
+				n = scanner.nextInt();
+				sum +=n;
+			}
+			catch(InputMismatchException e) {
+				System.out.println("정수가 아닙니다. 다시 입력하세요!");
+				scanner.next(); // 입력 스트임에 있는 정수가 아닌 토큰을 버림.
+				i--; // 인덱스가 증가하지 않도록 미리 
+			}
+		}
+		System.out.println("합은 "+sum);
+		scanner.close();
+	}
+}
