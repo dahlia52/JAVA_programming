a9d4d4e525f9e4734ba00f51a5e1a7b78471c6e3
dahlia52 <101353482+dahlia52@users.noreply.github.com>
2022-12-13 03:31:35 +0900

    Add files via upload

---
+++ Chapter13/ScannerTest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.File;
+
+public class ScannerTest {
+
+	public static void main(String[] args) {
+		int n;
+		try {
+			Scanner input = new Scanner(new File("numbers.txt"));
+			while (input.hasNext()) {
+				n = input.nextInt();
+				System.out.println(n);
+			}
+			input.close();
+		}
+		catch(FileNotFoundException e) {
+			System.err.println("cannot open");
+			System.exit(1);
+		}
+
+	}
+
+}
