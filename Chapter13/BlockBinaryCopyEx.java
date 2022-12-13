e940219afa0e3539bd7f7f017620bf496781d786
dahlia52 <101353482+dahlia52@users.noreply.github.com>
2022-12-14 08:37:03 +0900

    Add files via upload

---
+++ Chapter13/BlockBinaryCopyEx.java
@@ -0,0 +1,26 @@
+// 13-11 블록 단위로 바이너리 파일 고속 복사
+import java.io.*;
+
+public class BlockBinaryCopyEx {
+
+	public static void main(String[] args) {
+		File src = new File("c:\\Windows\\Web\\Wallpaper\\Theme1\\img1.jpg");
+		File dest = new File("c:\\Temp\\desert.jpg");
+		try {
+			FileInputStream fi = new FileInputStream(src);
+			FileOutputStream fo = new FileOutputStream(dest);
+			byte [] buf = new byte[1024*10]; // 10KB 버퍼
+			while (true) {
+				int n = fi.read(buf); // 버퍼크기만큼 읽기. n은 실제 읽은 바이트
+				fo.write(buf, 0, n);
+				if (n<buf.length) // 파일의 마지막 데이터
+					break;
+			}
+			fi.close(); fo.close();
+			System.out.println(src.getPath()+"를 "+dest.getPath()+"로 복사하였습니다.");
+		}
+		catch (IOException e) {
+			System.out.println("파일 복사 오류");
+		}
+	}
+}
