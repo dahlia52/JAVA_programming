994b7707b1336e9dc177106a02c257b4f41bc9c5
dahlia52 <101353482+dahlia52@users.noreply.github.com>
2022-12-13 23:24:04 +0900

    Add files via upload

---
+++ Chapter12/RunnableTimerEx.java
@@ -0,0 +1,44 @@
+// 12-2 Runnable 인터페이스를 이용하여 1초 단위 타이머 스레드 만들기
+import java.awt.*;
+import javax.swing.*;
+
+class TimerRunnable implements Runnable{
+	private JLabel timerLabel;
+	public TimerRunnable(JLabel timerLabel) {
+		this.timerLabel = timerLabel;
+	}
+	@Override
+	public void run() {
+		int n = 0;
+		while (true) {
+			timerLabel.setText(Integer.toString(n));
+			n++;
+			try {
+				Thread.sleep(1000);
+			}
+			catch (InterruptedException e) {return;}
+	}
+}
+}
+public class RunnableTimerEx extends JFrame {
+	public RunnableTimerEx() {
+		setTitle("Runnable을 구현한 타이머 스레드 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		JLabel timerLabel = new JLabel();
+		timerLabel.setFont(new Font("Gothic",Font.ITALIC, 80));
+		c.add(timerLabel);
+		
+		TimerRunnable runnable = new TimerRunnable(timerLabel);
+		Thread th = new Thread(runnable); // 스레드 객체 생성
+		
+		setSize(250,150);
+		setVisible(true);
+		th.start();
+	}
+	public static void main(String[] args) {
+		new RunnableTimerEx();
+	}
+}
---
+++ Chapter12/ThreadMainEx.java
@@ -0,0 +1,15 @@
+// 12-3 main 스레드 확인과 스레드 정보를 알아내는 코드
+public class ThreadMainEx {
+
+	public static void main(String[] args) {
+		long id = Thread.currentThread().getId();
+		String name = Thread.currentThread().getName();
+		int priority = Thread.currentThread().getPriority();
+		Thread.State s = Thread.currentThread().getState();
+		
+		System.out.println("현재 스레드 이름 = "+name);
+		System.out.println("현재 스레드 ID = "+id);
+		System.out.println("현재 스레드 우선순위 값 = "+priority);
+		System.out.println("현재 스레드 상태 = "+s);
+	}
+}
